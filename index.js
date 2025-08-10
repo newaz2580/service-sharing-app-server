1175c540e583b6f71581a4c37578b4676bb844de
newaz2796 <newaz2796@gmail.com>
2025-08-10 16:55:01 +0600

    change comment

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ app.use(
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yzyltda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+// Create a MongoClient with 
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
